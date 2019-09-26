5fb1cc149a8469fed38ea17e33bd300dc45bfaa3
Christian Benjaminsson <christiansdator@Christians-MacBook-Pro.local>
2019-09-26 18:39:51 +0200

    Push ProjektuppgiftDel1.java to Github

---
+++ ProjektuppgiftDel1.java
@@ -0,0 +1,13 @@
+package com.example.projektuppgiftdel1;
+
+import java.util.Scanner;
+
+public class ProjektuppgiftDel1 {
+
+    //Detta är den körbara klassen.
+    //Det ska finnas en meny med valen lägg till fordon och visa fordon samt avsluta applikationen.
+
+    public static void main(String[] args) {
+
+    }
+}
