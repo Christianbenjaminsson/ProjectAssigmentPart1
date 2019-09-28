bd4351102616835a9405016ca82e879c21ccd45d
Christian Benjaminsson <christiansdator@Christians-MacBook-Pro.local>
2019-09-28 09:27:26 +0200

    Push new class to Github

---
+++ Vehicle.java
@@ -0,0 +1,15 @@
+package com.example.projektuppgiftdel1;
+
+public class Vehicle {
+
+    private String brand;
+    private String color;
+    private int year;
+
+    public Vehicle(String brand, String color, int year) {
+        this.brand = brand;
+        this.color = color;
+        this.year = year;
+    }
+
+}
