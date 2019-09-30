168741db07fbae2b1dbbf61512daf18464692c5f
Christian Benjaminsson <christiansdator@Christians-MacBook-Pro.local>
2019-09-30 19:30:14 +0200

    New version

--- Car.java
+++ Car.java
@@ -5,10 +5,35 @@ public class Car extends Vehicle {
     private String typOfCar;
     private int numberOfPassengers;
 
+
     public Car(String brand, String color, int year, String typOfCar, int numberOfPassengers) {
         super(brand, color, year);
         this.typOfCar = typOfCar;
         this.numberOfPassengers = numberOfPassengers;
     }
 
+    public static Car createCar(String brand, String color, int year, String typOfCar, int numberOfPassengers) {
+        return new Car(brand, color, year, typOfCar, numberOfPassengers);
+    }
+
+    public String getBrand() {
+        return super.getBrand();
+    }
+
+    public String getColor() {
+        return super.getColor();
+    }
+
+    public int getYear() {
+        super.getYear()
+    }
+
+    public String getTypOfCar() {
+        return typOfCar;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
 }
+
