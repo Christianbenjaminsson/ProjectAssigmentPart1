093afc91f0470a0c08eee89102dd89af6ccb6906
Christian Benjaminsson <christiansdator@Christians-MacBook-Pro.local>
2019-09-30 19:31:15 +0200

    New version

--- ProjektuppgiftDel1.java
+++ ProjektuppgiftDel1.java
@@ -23,6 +23,7 @@ public class ProjektuppgiftDel1 {
 
             switch (input) {
                 case 1:
+                    addNewCar();
                 break;;
 
                 case 2:
@@ -43,4 +44,28 @@ public class ProjektuppgiftDel1 {
                 "3. End application");
 
     }
+
+    private static void addNewCar() {
+        System.out.println("Write brand of vehicle: ");
+        String brand = scan.nextLine();
+        System.out.println("Write color of vehicle: ");
+        String color = scan.nextLine();
+        System.out.println("Write year of vehicle: ");
+        int year = scan.nextInt();
+        scan.nextLine();
+        System.out.println("What kind of Vehicle: ");
+        String typOfCar = scan.nextLine();
+        System.out.println("Number of passengers: ");
+        int numberOfPassengers = scan.nextInt();
+        scan.nextLine();
+        Car newCar = Car.createCar(brand, color, year, typOfCar, numberOfPassengers);
+        if(VehiclesController.addNewCar(newCar)) {
+            System.out.println("New vehicle created: brand: " +brand+ " color: " +color+
+                    " year: " +year + " type of car: " +typOfCar+ " number of passengers " +numberOfPassengers);
+        } else {
+            System.out.println("Can´´ add vehicle " +brand+ " it already excists");
+        }
+    }
+
+
 }
