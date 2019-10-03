4d1b12da2dd9eb8fa31f5494793185362af312f5
Christian Benjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2019-10-03 10:16:48 +0200

    Push updated file

--- ProjektuppgiftDel1.java
+++ ProjektuppgiftDel1.java
@@ -4,13 +4,8 @@ import java.util.Scanner;
 
 public class ProjektuppgiftDel1 {
 
-    //Detta är den körbara klassen.
-    //Det ska finnas en meny med valen lägg till fordon och visa fordon samt avsluta applikationen.
-    
-    //Skapa Switch
-
     private static Scanner scan = new Scanner(System.in);
-    VehiclesController vehiclesController = new VehiclesController();
+    private static VehiclesController vehiclesController = new VehiclesController();
 
     public static void main(String[] args) {
 
@@ -18,52 +13,58 @@ public class ProjektuppgiftDel1 {
         printactions();
 
         while(!quit) {
-            System.out.println("Print your choise: ");
-            String input = scan.nextLine();
+            System.out.print("\nPrint your choise (choose 3 for menu): ");
+            int input = scan.nextInt();
+            scan.nextLine();
 
             switch (input) {
                 case 1:
                     addNewCar();
-                break;;
+                break;
 
                 case 2:
+                    vehiclesController.printVehicles();
                 break;
 
                 case 3:
-                quit = true;
+                    printactions();
+                break;
+
+                case 4:
+                    quit = true;
                 break;
             }
-
         }
     }
 
     private static void printactions() {
-        System.out.println("Print your choise: \n" +
+        System.out.print("\nMenu: \n\n" +
                 "1. Add vehicle \n" +
                 "2. Show vehicles \n" +
-                "3. End application");
+                "3. Show menu\n" +
+                "4. End application\n");
 
     }
 
     private static void addNewCar() {
-        System.out.println("Write brand of vehicle: ");
+        System.out.print("Write brand of vehicle: ");
         String brand = scan.nextLine();
-        System.out.println("Write color of vehicle: ");
+        System.out.print("Write color of vehicle: ");
         String color = scan.nextLine();
-        System.out.println("Write year of vehicle: ");
+        System.out.print("Write year of vehicle: ");
         int year = scan.nextInt();
         scan.nextLine();
-        System.out.println("What kind of Vehicle: ");
+        System.out.print("What kind of Vehicle: ");
         String typOfCar = scan.nextLine();
-        System.out.println("Number of passengers: ");
+        System.out.print("Number of passengers: ");
         int numberOfPassengers = scan.nextInt();
         scan.nextLine();
         Car newCar = Car.createCar(brand, color, year, typOfCar, numberOfPassengers);
-        if(VehiclesController.addNewCar(newCar)) {
-            System.out.println("New vehicle created: brand: " +brand+ " color: " +color+
-                    " year: " +year + " type of car: " +typOfCar+ " number of passengers " +numberOfPassengers);
+        if(vehiclesController.addNewCar(newCar)) {
+            System.out.println("New vehicle created: Brand: " +brand+ " Color: " +color+
+                    " Year: " +year + " Type of car: " +typOfCar+ " Number of passengers " +numberOfPassengers);
         } else {
-            System.out.println("Can´´ add vehicle " +brand+ " it already excists");
+            System.out.println("Can´t add vehicle " +brand+ " it already excists");
         }
     }
 
